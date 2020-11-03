1b76cb416a5778c1ff2bb0c13470fed5939691ae
jherberg462 <jherberg88@me.com>
2020-11-03 00:16:31 -0500

    refactor code

--- input_pipeline.py
+++ input_pipeline.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from params import params
 
 
 def decode_image_label(tfrec):
---
+++ params.py
@@ -0,0 +1,5 @@
+params = {
+    'batch_size' : 128,
+    'img_size' : [299, 299],
+    'epochs': 350
+}
